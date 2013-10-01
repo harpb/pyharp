b7ccf7588f9de0f4515b3cb2458ca1ef22f7619a
Harp. B <hi@harpb.com>
2013-10-01 14:25:49 -0700

    + Using disutil for setup.py

--- setup.py
+++ setup.py
@@ -3,24 +3,21 @@ import sys, os
 
 version = '0.1'
 
-setup(name='pyHarp',
-      version=version,
-      description="Tools for python",
-      long_description="""\
+setup(name = 'pyHarp',
+      version = version,
+      description = "Tools for python",
+      long_description = """\
 These set of tools...""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='file, move, remove',
-      author='HarpB',
-      author_email='hi@harpb.com',
-      url='https://github.com/harpb/pyharp',
-      license='BSD',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=True,
-      install_requires=[
-          # -*- Extra requirements: -*-
+      classifiers = [],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords = 'file, move, remove',
+      author = 'HarpB',
+      author_email = 'hi@harpb.com',
+      url = 'https://github.com/harpb/pyharp',
+      license = 'BSD',
+      include_package_data = True,
+      zip_safe = True,
+      install_requires = [
+          'django'
       ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
+      packages =['pyharp'],
       )
